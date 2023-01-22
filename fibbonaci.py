ab15c3a0035a8c6f84834e48ecdaadb92ec21a92
fatimazariwala <82675386+fatimazariwala@users.noreply.github.com>
2023-01-22 13:35:21 +0530

    Update fibbonaci.py

--- fibbonaci.py
+++ fibbonaci.py
@@ -1,1 +1,20 @@
 # python
+terms=int(input("Enter number of terms you want to print :  "))
+n1=0
+n2=1
+count=0
+
+if terms<=0:
+    print("Please enter number of terms to be printed ...")
+
+elif terms==1:
+    print(n1)
+else :
+    print("Fibonacci Sequence : ")
+    while count<terms:
+        print(n1)
+        nth=n1+n2
+        n1=n2
+        n2=nth
+        count += 1
+    
